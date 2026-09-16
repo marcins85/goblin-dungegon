bfed46eed2a6c22679d4e8365fe022a0e149ec29
marcins85 <stassimon@gmail.com>
2026-09-16 08:33:16 +0200

    fix: set throwed item as pickable

--- scenes/equipment/ThrowedItem.cs
+++ scenes/equipment/ThrowedItem.cs
@@ -1,8 +1,11 @@
 using Godot;
 using System;
 
-public partial class ThrowedItem : Node3D, IPickable
+public partial class ThrowedItem : RigidBody3D, IPickable
 {
+	private static readonly PackedScene PickableItemPrefab =
+		GD.Load<PackedScene>("res://scenes/equipment/pickable_item.tscn");
+
 	[Export]
 	private WeaponData _weaponData;
 	private CollisionShape3D _collision;
@@ -32,7 +35,32 @@ public partial class ThrowedItem : Node3D, IPickable
 				AddChild(thrownObject);
 				var meshNode = thrownObject.GetChild<MeshInstance3D>(0);
 				_collision.Shape = meshNode.Mesh.CreateConvexShape();
+				BodyEntered += OnBodyEntered;
 			}
 		}
+	}
+
+	private bool sleepingChanged = false;
+    private void OnBodyEntered(Node body)
+    {
+		if (!sleepingChanged)
+		{
+			SleepingStateChanged += OnSleep;
+			sleepingChanged = true;
+		}
+
+		// if (!IsConnected("sleeping_state_changed", new Callable(this, nameof(OnSleep))))
+		// {
+        // 	Connect("sleeping_state_changed", new Callable(this, nameof(OnSleep)));
+		// }
+    }
+
+	private void OnSleep()
+	{
+		var pickableItem = PickableItemPrefab.Instantiate<PickableItem>();
+		pickableItem.WeaponData = _weaponData;
+		pickableItem.GlobalTransform = GlobalTransform;
+		GetTree().GetRoot().AddChild(pickableItem);
+		QueueFree();
 	}
 }
