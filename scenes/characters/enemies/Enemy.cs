71762740f4d86a3bcda85653aa217196b2ac9411
Marcin Stachowicz <stassimon@gmail.com>
2026-09-16 12:30:23 +0200

    feat: add ragdoll physics for enemies

--- scenes/characters/enemies/Enemy.cs
+++ scenes/characters/enemies/Enemy.cs
@@ -6,11 +6,15 @@ public partial class Enemy : CharacterBody3D
     private static readonly PackedScene EquipedItemPrefab =
         GD.Load<PackedScene>("res://scenes/equipment/equiped_item.tscn");
 
-    private Node3D _torso;
+    private PhysicalBone3D _torso;
+    private PhysicalBoneSimulator3D _skeletonSimulator;
+    private CollisionShape3D _collision;
 
     public override void _Ready()
     {
-        _torso = GetNode<Node3D>("%Physical Bone Torso");
+        _torso = GetNode<PhysicalBone3D>("%Physical Bone Torso");
+        _skeletonSimulator = GetNode<PhysicalBoneSimulator3D>("%PhysicalBoneSimulator3D");
+        _collision = GetNode<CollisionShape3D>("CollisionShape3D");
     }
 
     public void Impale(ThrowedItem thrownItem, Basis basis)
@@ -24,6 +28,14 @@ public partial class Enemy : CharacterBody3D
             impaledItem.TranslateObjectLocal(impaledItem.WeaponData.impaleLocalTranslation);
             impaledItem.RotateObjectLocal(Vector3.Up, impaledItem.WeaponData.imapleLocalRotation);
             thrownItem.QueueFree();
+            RegisterDeath();
         }
+    }
+
+    private void RegisterDeath()
+    {
+        _collision.Disabled = true;
+        _skeletonSimulator.Active = true;
+        _skeletonSimulator.PhysicalBonesStartSimulation();
     }
 }
